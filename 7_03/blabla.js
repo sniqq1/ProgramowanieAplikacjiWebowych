df3e4a743d3d97bd66a843996c289da0d4c908e4
sniqq1 <pat.wojtaszek@gmail.com>
2021-03-20 09:54:35 +0100

    podstawy

--- 7_03/blabla.js
+++ 7_03/blabla.js
@@ -1,3 +1,45 @@
-var header = document.createElement('div');
-header.innerHTML = "lalala";
-document.body.appendChild(header);
+var Pab = /** @class */ (function () {
+    function Pab() {
+        this.start();
+    }
+    Pab.prototype.start = function () {
+        this.load();
+        this.watch();
+    };
+    Pab.prototype.load = function () {
+        this.input1input = document.querySelector('#input1');
+        this.input2input = document.querySelector('#input2');
+        this.input3input = document.querySelector('#input3');
+        this.input4input = document.querySelector('#input4');
+        this.suminput = document.querySelector('#sum');
+        this.avginput = document.querySelector('#avg');
+        this.mininput = document.querySelector('#min');
+        this.maxinput = document.querySelector('#max');
+    };
+    Pab.prototype.watch = function () {
+        var _this = this;
+        this.input1input.addEventListener('input', function () { return _this.countData(); });
+        this.input2input.addEventListener('input', function () { return _this.countData(); });
+        this.input3input.addEventListener('input', function () { return _this.countData(); });
+        this.input4input.addEventListener('input', function () { return _this.countData(); });
+    };
+    Pab.prototype.countData = function () {
+        var data1 = +this.input1input.value;
+        var data2 = +this.input2input.value;
+        var data3 = +this.input3input.value;
+        var data4 = +this.input4input.value;
+        var sum = data1 + data2 + data3 + data4;
+        var avg = sum / 4;
+        var min = Math.min(data1, data2, data3, data4);
+        var max = Math.max(data1, data2, data3, data4);
+        this.show(sum, avg, min, max);
+    };
+    Pab.prototype.show = function (sum, avg, min, max) {
+        this.suminput.value = sum.toString();
+        this.avginput.value = avg.toString();
+        this.mininput.value = min.toString();
+        this.maxinput.value = max.toString();
+    };
+    return Pab;
+}());
+var hejka = new Pab();
--- 7_03/blabla.ts
+++ 7_03/blabla.ts
@@ -1,4 +1,61 @@
-const header = document.createElement('div');
-header.innerHTML = "lalala";
+class Pab {
+    input1input : HTMLInputElement;
+    input2input: HTMLInputElement;
+    input3input: HTMLInputElement;
+    input4input: HTMLInputElement;
+    suminput: HTMLInputElement;
+    avginput: HTMLInputElement;
+    mininput: HTMLInputElement;
+    maxinput: HTMLInputElement;
 
-document.body.appendChild(header);
+    constructor() {
+        this.start();
+    }
+
+    start() {
+        this.load();
+        this.watch();
+    }
+
+    load() {
+        this.input1input = document.querySelector('#input1');
+        this.input2input = document.querySelector('#input2');
+        this.input3input = document.querySelector('#input3');
+        this.input4input = document.querySelector('#input4');
+        this.suminput = document.querySelector('#sum');
+        this.avginput = document.querySelector('#avg');
+        this.mininput = document.querySelector('#min');
+        this.maxinput = document.querySelector('#max');
+    }
+
+    watch() {
+        this.input1input.addEventListener('input', () => this.countData());
+        this.input2input.addEventListener('input', () => this.countData());
+        this.input3input.addEventListener('input', () => this.countData());
+        this.input4input.addEventListener('input', () => this.countData());
+    }
+
+    countData(){
+        const data1  = +this.input1input.value;
+        const data2 = +this.input2input.value;
+        const data3 = +this.input3input.value;
+        const data4 = +this.input4input.value;
+
+        const sum = data1+data2+data3+data4;
+        const avg = sum /4;
+        const min = Math.min(data1,data2,data3,data4);
+        const max = Math.max(data1, data2, data3, data4);
+        this.show(sum, avg, min, max);
+    }
+    show(sum: number, avg: number, min: number, max: number) {
+        this.suminput.value = sum.toString();
+        this.avginput.value = avg.toString();
+        this.mininput.value = min.toString();
+        this.maxinput.value = max.toString();
+
+    }
+
+
+}
+
+const hejka = new Pab();
