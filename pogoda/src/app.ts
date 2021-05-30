744c5fa9ccc894d18ac8afc39503798c20a0e60e
sniqq1 <pat.wojtaszek@gmail.com>
2021-05-30 18:12:29 +0200

    omg

--- pogoda/src/app.ts
+++ pogoda/src/app.ts
@@ -1,24 +1,39 @@
+import { IWeather, IWeatherData } from './interface';
+
+
+
 export class App {
     apiKey = "a3bc515e60ed338d4c59cc163b1c1f96";
+ 
 
     constructor() {
-        this.getCityInfo('warszawa');
+      this.load();
+
+    }
+    
+    async getCityInfo(city?: string) {
+        
+        if(city) {
+            const weather = await this.getWeather(city);
+            if(weather && weather.cod === 200) {
+                this.saveData(weather);
+            }
+
+            return weather;
+        } 
     }
 
-    async getCityInfo(city: string) {
-        const weather = await this.getWeather(city);
-        this.saveData(weather);
-    }
-
-    async getWeather(city: string): Promise<any> {
+    async getWeather(city: string): Promise<IWeatherData> {
         const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.apiKey}`;
         const weatherResponse = await fetch(openWeatherUrl);
         const weatherData = await weatherResponse.json();
+
         console.log(weatherData);
+  
         return weatherData;
     }
 
-    saveData(data: any) {
+    saveData(data: IWeatherData) {
             localStorage.setItem('weatherData', JSON.stringify(data));
     }
 
@@ -31,7 +46,21 @@ export class App {
             return{};
         }
     }
+    load() {
+    const cityButton: HTMLButtonElement = document.querySelector('#cityButton');
+    const cityInput = document.getElementById('cityInput') as HTMLInputElement;
+   
+    
+    
+    cityButton.addEventListener('click', () => {
 
 
-
+        const nameCity: string = cityInput.value;
+        console.log(nameCity);
+        this.getCityInfo(nameCity);
+    });
+    }
 }
+
+
+
---
+++ pogoda/src/interface.ts
@@ -0,0 +1,32 @@
+export interface IWeather {
+    id: number,
+    main: string,
+    description: string,
+    icon: string
+}
+
+export interface IWeatherData {
+    coord: {
+        lon: number,
+        lat: number,
+    },
+    weather: IWeather[],
+    base: string,
+    main: {
+        temp: number,
+        feels_like: number,
+        temp_min: number
+        temp_max: number
+        pressure: number
+        humidity: number
+    },
+    visibility: number,
+    wind: {
+        speed: number,
+        deg: number,
+    }
+    timezone: number,
+    id: number,
+    name: string,
+    cod: number
+}
